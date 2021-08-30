dd16fe293d7474cd0cb66986dc9b2dcf59fc8d9b
TheE7Player <36343624+TheE7Player@users.noreply.github.com>
2021-08-30 19:03:54 +0100

    removed force error testing

--- SteamCMDLauncher/Views/main_view.xaml.cs
+++ SteamCMDLauncher/Views/main_view.xaml.cs
@@ -250,8 +250,6 @@ namespace SteamCMDLauncher
         {
             Config.Log("[MV] Window has been fully loaded");
 
-            throw new Exception("This is a forced error");
-
             if (out_of_date)
             {
                 Config.Log("[MV] Showing update dialog to the client");
