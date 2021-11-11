add668d21ee0997f55fb3d4faa21009ebdef5ce0
TheE7Player <36343624+TheE7Player@users.noreply.github.com>
2021-11-11 13:07:56 +0000

    More working Archive Class
    It can now create folders in temp and in zip

--- SteamCMDLauncher/Component/Archive.cs
+++ SteamCMDLauncher/Component/Archive.cs
@@ -9,29 +9,53 @@ namespace SteamCMDLauncher.Component
 {
     public class Archive
     {
+        /// <summary>
+        /// Instructs the class if its good to clean up the data
+        /// </summary>
+        public bool Cleanup { get; set; }
+
+        private bool IsLoaded;
+
         private ArchiveData self_data;
 
         private const int COMPRESS_LEVEL = 9;
+
+        /// <summary>
+        /// TMP folder for extracting or reading the files
+        /// </summary>
         public static string CACHE_PATH = Path.Combine(Path.GetTempPath(), "TheE7Player", "SteamCMDLauncher", "temp");
 
-        private string DEFAULT_EXTENTION_CFG = ".smdcg";
-        private string DEFAULT_EXTENTION_SETTING = ".smds";
-        private string META_DATA_FILE = ".metadata";
-        private string LOCALIZATION_PATH = "/lang/";
+        private string DEFAULT_EXTENTION_CFG = ".smdcg",
+        DEFAULT_EXTENTION_SETTING = ".smds",
+        META_DATA_FILE = ".metadata",
+        LOCALIZATION_PATH = "/lang/";
 
         ~Archive()
         {
-            DEFAULT_EXTENTION_CFG = null;
-            DEFAULT_EXTENTION_SETTING = null;
-            META_DATA_FILE = null;
-            LOCALIZATION_PATH = null;
+            ForceClear();
         }
 
         public Archive(string file_location, string appid, bool performLoad = false)
         {
+            IsLoaded = false;
+
             this.self_data = new ArchiveData();
             this.self_data.locationReference = file_location;
             this.self_data.GameID = appid;
+            this.self_data.tempReference = Path.Combine(CACHE_PATH, Path.GetFileNameWithoutExtension(this.self_data.locationReference));
+
+            // Create the tmp folder if it doesn't exist already
+            if (!Directory.Exists(CACHE_PATH))
+            {
+                Config.Log($"[AD] Creating TMP folder in: {CACHE_PATH}");
+                Directory.CreateDirectory(CACHE_PATH);
+            }
+
+            // Clean up any old file (if any)
+            if (Directory.Exists(this.self_data.tempReference))
+            {
+                Directory.Delete(this.self_data.tempReference, true);
+            }
 
             if (!File.Exists(file_location))
             { 
@@ -43,6 +67,37 @@ namespace SteamCMDLauncher.Component
             }
 
             file_location = null; appid = null;
+        }
+        
+        private void Clear()
+        {
+            if (Directory.Exists(self_data.tempReference))
+                Directory.Delete(self_data.tempReference, true);
+        }
+
+        public void ForceClear()
+        {
+            Console.WriteLine("AAAAAAAAAAAA");
+
+            DEFAULT_EXTENTION_CFG = null;
+            DEFAULT_EXTENTION_SETTING = null;
+            META_DATA_FILE = null;
+            LOCALIZATION_PATH = null;
+
+            if (Cleanup && IsLoaded)
+            {
+                Console.WriteLine("AAAAAAAAAAAA x2");
+                Clear();
+            }
+        }
+
+        [Obsolete("Maybe remove if this method has no purpose")]
+        private void InitialseConstants()
+        {
+             DEFAULT_EXTENTION_CFG = ".smdcg";
+             DEFAULT_EXTENTION_SETTING = ".smds";
+             META_DATA_FILE = ".metadata";
+             LOCALIZATION_PATH = "/lang/";   
         }
 
         private byte[] CreateMetaDataFile()
@@ -67,9 +122,14 @@ namespace SteamCMDLauncher.Component
             return output;
         }
 
-        //Dictionary<string, string[]> colection
         public void SaveFile()
         {
+            if (!Directory.Exists(this.self_data.tempReference))
+            {
+                Config.Log($"[AD] Creating ARCHIVE TMP folder: {this.self_data.tempReference}");
+                Directory.CreateDirectory(this.self_data.tempReference);
+            }
+
             this.self_data.Revisions++;
 
             MemoryStream outputMemStream = new MemoryStream();
@@ -80,39 +140,50 @@ namespace SteamCMDLauncher.Component
             byte[] bytes = null;
 
             // Create the metadata file first
-            var metadata_entry = new ZipEntry(META_DATA_FILE);
-            metadata_entry.DateTime = DateTime.Now;
+
+            DateTime dateProcessed = DateTime.Now;
+
+            ZipEntry metadata_entry = new ZipEntry(META_DATA_FILE);
+            metadata_entry.DateTime = dateProcessed;
 
             zipStream.PutNextEntry(metadata_entry);
 
             bytes = CreateMetaDataFile();
 
             zipStream.Write(bytes, 0, bytes.Length);
+           
+            string realitive = string.Empty;
 
+            // Now we do each other file stored in temp
+            foreach (string file in Directory.GetFiles(self_data.tempReference, "*.*", SearchOption.AllDirectories))
+            {
+                if (Path.GetFileName(file) == ".metadata") continue;
+
+                realitive = Path.GetRelativePath(self_data.tempReference, file);
+                
+                metadata_entry = new ZipEntry(realitive);
+                metadata_entry.DateTime = dateProcessed;
+                
+                zipStream.PutNextEntry(metadata_entry);
+
+                bytes = File.ReadAllBytes(file);
+                zipStream.Write(bytes, 0, bytes.Length);
+            }
+            
             zipStream.IsStreamOwner = false;
             zipStream.Close();
-
+            realitive = null;
             System.IO.File.WriteAllBytes(this.self_data.locationReference, outputMemStream.GetBuffer());
         }
 
         public void LoadFile()
         {
-            if (!Directory.Exists(CACHE_PATH)) 
+
+            if(!Directory.Exists(this.self_data.tempReference))
             {
-                Config.Log($"[AD] Creating TMP folder in: {CACHE_PATH}");
-                Directory.CreateDirectory(CACHE_PATH); 
+                Config.Log($"[AD] Creating ARCHIVE TMP folder: {this.self_data.tempReference}");
+                Directory.CreateDirectory(this.self_data.tempReference);
             }
-
-            string zip_path_folder = Path.Combine(CACHE_PATH, Path.GetFileNameWithoutExtension(this.self_data.locationReference));
-
-            if(Directory.Exists(zip_path_folder))
-            {
-                Directory.Delete(zip_path_folder, true);
-            }
-
-            Directory.CreateDirectory(zip_path_folder);
-
-            this.self_data.tempReference = zip_path_folder;
 
             using (var fs = new FileStream(this.self_data.locationReference, FileMode.Open, FileAccess.Read))
             {
@@ -156,14 +227,101 @@ namespace SteamCMDLauncher.Component
                             }                
                         }
 
-                        File.WriteAllText(Path.Combine(zip_path_folder, ze.Name), fi);
+                        File.WriteAllText(Path.Combine(this.self_data.tempReference, ze.Name), fi);
                         
                         fi = null;
                     }
                 }
             }
-           
-           // return this.self_data;
+
+            IsLoaded = true;
+        }
+
+        /// <summary>
+        /// A type which returns a result based on if it was able to read the tmp file required
+        /// </summary>
+        /// <param name="target">The internal file to attempt to access</param>
+        /// <returns>A tuple, T1 being the result - T2 being the error reason or content of the file</returns>
+        public (int, string) GetFileContents(string target)
+        {
+            string temp_contents = string.Empty;
+            try
+            {
+                if (!IsLoaded) 
+                    return (-1, "No archive was loaded to read from a file.");
+
+                if(!Directory.Exists(self_data.tempReference))
+                    return (-1, "The requested temp file folder does not exist");
+
+                temp_contents = Path.Combine(self_data.tempReference, target);
+
+                if (!File.Exists(temp_contents))
+                    return (-1, "The requested temp file folder does not exist");
+
+                return (0, File.ReadAllText(temp_contents));
+
+            }
+            finally
+            {
+                target = null;
+                temp_contents = null;
+            }        
+        }
+    
+        public (bool, string) SetFileContents(string target, string contents)
+        {
+            string temp_contents = string.Empty;
+
+            try
+            {
+                if (!IsLoaded)
+                    return (false, "No archive was loaded to read from a file.");
+
+                if (!Directory.Exists(self_data.tempReference))
+                    return (false, "The requested temp file folder does not exist");
+
+                // Now we process the query, Folder depth is denoted by '>'
+                if(target.Contains('>'))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    string[] structure = target.Trim().Split('>');
+
+                    string path = string.Empty;
+                    int idx = 0;
+                    int depth = structure.Length - 1;
+
+                    while (idx != depth)
+                    {
+                        path = Path.Combine(self_data.tempReference, structure[idx]);
+
+                        if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+                        
+                        sb.Append($"{structure[idx]}/");
+
+                        idx++;
+                    }
+                    sb.Append(structure[idx]);
+                    temp_contents = Path.Combine(self_data.tempReference, sb.ToString());
+                    structure = null;
+                    sb = null;
+                    path = null;
+                }
+                else
+                {
+                    temp_contents = Path.Combine(self_data.tempReference, target);
+                }
+
+                File.WriteAllText(temp_contents, contents);
+
+                return (true, string.Empty);
+
+            }
+            finally
+            {
+                target = null;
+                contents = null;
+                temp_contents = null;
+            }
         }
     }
 
--- SteamCMDLauncherDriverTest/Program.cs
+++ SteamCMDLauncherDriverTest/Program.cs
@@ -9,7 +9,14 @@ namespace SteamCMDLauncherDriverTest
         {
             string target = @"C:\Users\james\Desktop\file_test.zip";
 
+            if (!System.IO.File.Exists(target))
+                CreateInstace(target);
+
             DoSave(target);
+
+            DoFileFetch(target, ".metadata");
+
+            DoFileSaveAmend(target, ".metadata", "example_folder>.new_file_data", "This has been written");
 
         }
 
@@ -29,6 +36,36 @@ namespace SteamCMDLauncherDriverTest
             var aaa = new Archive(target, "740", true);
             
             aaa.SaveFile();
+        }
+    
+        static void DoFileFetch(string target, string file)
+        {
+            var aaa = new Archive(target, "740", true);
+
+            var result = aaa.GetFileContents(file);
+
+            Console.WriteLine("Result Number: {0}, Result Return: {1}", result.Item1, result.Item2);
+
+            file = null;
+            target = null;
+        }
+
+        static void DoFileSaveAmend(string target, string file, string new_file, string new_file_content)
+        {
+            var aaa = new Archive(target, "740", true);
+
+            var result = aaa.GetFileContents(file);
+
+            var result2 = aaa.SetFileContents(new_file, new_file_content);
+
+            Console.WriteLine(result2.Item1 ? "Successful Write" : "Failed Write");
+
+            aaa.SaveFile();
+
+            file = null;
+            target = null;
+            new_file = null; 
+            new_file_content = null;
         }
     }
 }
